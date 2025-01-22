18f72b21e4e261651c239a07c61c178e3fd134e1
Rainers Lovkins <rainerslovkins@gmail.com>
2025-01-22 18:33:51 +0200

    create the sign up page and form

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -8,3 +8,10 @@ export const signInDefaultValues = {
   email: '',
   password: '',
 };
+
+export const signUpDefaultValues = {
+  name: 'Steve Smith',
+  email: 'steve@smith.com',
+  password: 'password',
+  confirmPassword: 'password',
+};
