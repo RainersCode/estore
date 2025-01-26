092d8088cf98426b94c6f8fbac6c67a4c6dd5603
Rainers Lovkins <rainerslovkins@gmail.com>
2025-01-26 17:01:53 +0200

    create cart schema and model

--- types/index.ts
+++ types/index.ts
@@ -1,12 +1,19 @@
-import { z } from 'zod';
-import { insertProductSchema } from '@/lib/validators';
+import { z } from "zod";
+import {
+  cartItemSchema,
+  insertCartSchema,
+  insertProductSchema,
+} from "@/lib/validators";
 
-export type Product = Omit<z.infer<typeof insertProductSchema>, 'price'> & {
-    id: string;
-    createdAt: Date;
-    rating: number;
-    numReviews: number;
-    price: number;
-    image?: string;
-    inStock?: boolean;
+export type Product = Omit<z.infer<typeof insertProductSchema>, "price"> & {
+  id: string;
+  createdAt: Date;
+  rating: number;
+  numReviews: number;
+  price: number;
+  image?: string;
+  inStock?: boolean;
 };
+
+export type CartItem = z.infer<typeof cartItemSchema>;
+export type Cart = z.infer<typeof insertCartSchema>;
